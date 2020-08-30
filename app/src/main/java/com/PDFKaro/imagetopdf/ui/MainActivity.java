9dcbcaf4d5a7ee06f2d4dc3b5df90460ec739711
Adityaverma123 <av9567@gmail.com>
2020-08-30 20:11:38 +0530

    added toolbar

--- app/src/main/java/com/PDFKaro/imagetopdf/ui/MainActivity.java
+++ app/src/main/java/com/PDFKaro/imagetopdf/ui/MainActivity.java
@@ -1,15 +1,5 @@
 package com.PDFKaro.imagetopdf.ui;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
@@ -18,42 +8,32 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.PDFKaro.imagetopdf.Fragments.AddFileFragment;
-import com.PDFKaro.imagetopdf.Fragments.ListFragments;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.PDFKaro.imagetopdf.Fragments.AddImageFragment;
+import com.PDFKaro.imagetopdf.Fragments.HomeFragment;
+import com.PDFKaro.imagetopdf.Interface.RefreshList;
 import com.PDFKaro.imagetopdf.R;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity implements AddFileFragment.RefreshList {
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    AddFileFragment fileFragment;
-    ListFragments listFragments;
+public class MainActivity extends AppCompatActivity implements RefreshList {
+    AddImageFragment imageFragment;
+    HomeFragment homeFragment;
     Toolbar toolbar;
     ConstraintLayout main_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tabLayout = findViewById(R.id.tablayout);
-        viewPager = findViewById(R.id.view_pager);
         toolbar=findViewById(R.id.toolbar1);
         main_layout=findViewById(R.id.main_layout);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
-        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
-         fileFragment=new AddFileFragment();
-         listFragments=new ListFragments();
-        adapter.addFragment(fileFragment,"Add File");
-        adapter.addFragment(listFragments,"Your PDFs");
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(2);
-        tabLayout.setupWithViewPager(viewPager);
+        imageFragment=new AddImageFragment();
+        homeFragment=new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,homeFragment,null).commit();
     }
 
     @Override
@@ -110,47 +90,7 @@ public class MainActivity extends AppCompatActivity implements AddFileFragment.R
 
     @Override
     public void sendName(String name,String uri) {
-        String tag = "android:switcher:" + R.id.view_pager   + ":" + 1;
-        ListFragments f=(ListFragments)getSupportFragmentManager().findFragmentByTag(tag);
-        f.addReceivedName(name,uri);
-    }
 
-
-
-    private class ViewPagerAdapter extends FragmentPagerAdapter {
-        ArrayList<String> fragmentTitleList=new ArrayList<>();
-        List<Fragment> fragmentList=new ArrayList<>();
-        private Map<Integer, String> mFragmentTags;
-        private FragmentManager mFragmentManager;
-        public void addFragment(Fragment fragment,String title)
-        {
-            fragmentTitleList.add(title);
-            fragmentList.add(fragment);
-        }
-        public ViewPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-            mFragmentManager = fm;
-            mFragmentTags = new HashMap<Integer, String>();
-        }
-
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            return fragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return fragmentList.size();
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return fragmentTitleList.get(position);
-        }
-
-
+        homeFragment.addReceivedName(name,uri);
     }
 }
